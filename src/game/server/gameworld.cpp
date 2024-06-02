a202751ada610740d1950a68e0c83d1afa144fd3
Tater <Mr.Potatooh@gmail.com>
2024-06-02 17:08:51 -0500

    Preserve client order of weapon physics

--- src/game/server/gameworld.cpp
+++ src/game/server/gameworld.cpp
@@ -6,6 +6,7 @@
 #include "entity.h"
 #include "gamecontext.h"
 #include "gamecontroller.h"
+#include "player.h"
 
 #include <engine/shared/config.h>
 
@@ -214,13 +215,15 @@ void CGameWorld::Tick()
 		if(GameServer()->m_pController->IsForceBalanced())
 			GameServer()->SendChat(-1, TEAM_ALL, "Teams have been balanced");
 
-		// This is placed here so that certain weapon physics can happen before the regular Charecter Tick() to preserve physics accuracy
-		auto *pChar = m_apFirstEntityTypes[ENTTYPE_CHARACTER];
-		for(; pChar;)
+		// This is placed here so that certain weapon physics can happen before the regular Charecter Tick() to preserve physics accuracy.
+		// It is done in client order to preserve previous behavior.
+		for(auto &pPlayer : GameServer()->m_apPlayers)
 		{
-			m_pNextTraverseEntity = pChar->m_pNextTypeEntity;
-			((CCharacter *)pChar)->WeaponTick();
-			pChar = m_pNextTraverseEntity;
+			if(!pPlayer)
+				continue;
+			CCharacter *pChar = pPlayer->GetCharacter();
+			if(pChar)
+				pChar->WeaponTick();
 		}
 
 		// update all objects
